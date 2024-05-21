4488dc752d174d2fe382ab5ffbb1fab63fab22d8
hll <2733017657@qq.com>
2024-05-21 21:51:36 +0800

    向 UserDAO 添加方法

---
+++ UserDAO.java
@@ -0,0 +1,7 @@
+public class UserDAO {
+    public void insert() {}
+    public void delete() {}
+    public void update() {}
+    public void selectAll() {}
+    public void selectByUsername() {}
+}
